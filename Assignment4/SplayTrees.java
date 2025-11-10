5a5ccc416dd3c11f4bef8af0fa09fb63c1eeba33
Sonal Kamble <sonalkamble@MacBookPro.lan>
2025-11-09 20:37:32 -0500

    Splay Trees

--- Assignment4/SplayTrees.java
+++ Assignment4/SplayTrees.java
@@ -7,7 +7,7 @@ public class SplayTrees
     public static void main(String[] args) {
 
         Scanner sc;
-        
+
         try{
             sc = new Scanner(new File("/Users/sonalkamble/Desktop/Sonal/Desktop/ADS - Kequin Li/Assignment4/in.dat"));
         } catch (FileNotFoundException e) {
@@ -20,8 +20,63 @@ public class SplayTrees
         while(sc.hasNextLine())
         {
             String str = sc.nextLine();
+
+            Node root = new Node(Integer.parseInt(str), null, null);
+
             System.out.println(str);
         }
         System.out.println("Bye-bye!");
+    }
+}
+
+
+class Node
+{
+    private Integer data;
+    private Node left;
+    private Node right;
+
+    public Integer getData() {
+        return data;
+    }
+
+    public void setData(Integer data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Node(Integer data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
+    public void addAfter(Node root)
+    {
+
     }
 }
