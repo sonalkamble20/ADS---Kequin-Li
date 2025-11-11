75b9ffe291a98ee4a6df7eec8a1c480f4aa6c894
Sonal Kamble <sonalkamble@MacBookPro.lan>
2025-11-10 23:44:49 -0500

    splay trees update

--- Assignment4/SplayTrees.java
+++ Assignment4/SplayTrees.java
@@ -4,7 +4,30 @@ import java.util.Scanner;
 
 public class SplayTrees
 {
-    public static void main(String[] args) {
+    Node root;
+
+    void insert(int value)
+    {
+        root = recursiveInsert(root, value);
+    }
+
+    Node recursiveInsert(Node root, int value)
+    {
+        if(root == null)
+            root = new Node(value);
+
+        else if(value < root.data)
+            root.left = recursiveInsert(root.left, value);
+        else
+            root.right = recursiveInsert(root.right, value);
+
+        return root;
+    }
+
+
+    public static void main(String[] args)
+    {
+        SplayTrees st = new SplayTrees();
 
         Scanner sc;
 
@@ -21,62 +44,26 @@ public class SplayTrees
         {
             String str = sc.nextLine();
 
-            Node root = new Node(Integer.parseInt(str), null, null);
+            st.insert(Integer.parseInt(str));
 
             System.out.println(str);
         }
         System.out.println("Bye-bye!");
+
     }
+
+
 }
 
 
 class Node
 {
-    private Integer data;
-    private Node left;
-    private Node right;
+    Integer data;
+    Node left;
+    Node right;
 
-    public Integer getData() {
-        return data;
-    }
-
-    public void setData(Integer data) {
+    public Node(Integer data) {
         this.data = data;
-    }
-
-    public Node getLeft() {
-        return left;
-    }
-
-    public void setLeft(Node left) {
-        this.left = left;
-    }
-
-    public Node getRight() {
-        return right;
-    }
-
-    public void setRight(Node right) {
-        this.right = right;
-    }
-
-    public Node(Integer data, Node left, Node right) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
-    }
-
-    @Override
-    public String toString() {
-        return "Node{" +
-                "data=" + data +
-                ", left=" + left +
-                ", right=" + right +
-                '}';
-    }
-
-    public void addAfter(Node root)
-    {
-
+        left = right = null;
     }
 }
